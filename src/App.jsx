730de917bda36c7b135c72bde980fddccef940ca
Michael Feleke <felekemichael999@gmail.com>
2023-12-13 12:59:37 +0300

    Building the pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,26 @@
-function App() {
-  return <div>WorldWise</div>;
-}
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import HomePage from "./Pages/HomePage";
+import Pricing from "./Pages/Pricing";
+import Product from "./Pages/Product";
+import PageNotFound from "./Pages/PageNotFound";
+import AppLayout from "./Pages/AppLayout";
+import Login from "./Pages/Login";
 
+function App() {
+  return (
+    <>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/app" element={<AppLayout />} />
+          <Route path="/pricing" element={<Pricing />} />
+          <Route path="/product" element={<Product />} />
+          <Route path="/product" element={<Product />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="*" element={<PageNotFound />} />
+        </Routes>
+      </BrowserRouter>
+    </>
+  );
+}
 export default App;
