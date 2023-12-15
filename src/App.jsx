e04a7d03671487b9690213ccfec3b68ac2e2c531
Michael Feleke <felekemichael999@gmail.com>
2023-12-15 19:03:58 +0300

    Protecting the Route

--- src/App.jsx
+++ src/App.jsx
@@ -12,6 +12,7 @@ import City from "./Components/City";
 import Form from "./Components/Form";
 import { CitiesProvider } from "./Contexts/CitiesContext";
 import { AuthProvider } from "./Contexts/FakeAuthContext";
+import ProtectedRoute from "./Pages/ProtectedRoute";
 
 function App() {
   return (
@@ -24,7 +25,14 @@ function App() {
 
             <Route path="product" element={<Product />} />
             <Route path="login" element={<Login />} />
-            <Route path="app" element={<AppLayout />}>
+            <Route
+              path="app"
+              element={
+                <ProtectedRoute>
+                  <AppLayout />
+                </ProtectedRoute>
+              }
+            >
               <Route index element={<Navigate replace to="cities" />} />
               <Route path="cities" element={<CityList />} />
               <Route path="cities/:id" element={<City />} />
